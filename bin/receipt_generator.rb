9d35711798dd92257ddf26056df01d97dfc42ba6
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2025-05-01 07:19:40 -0400

    Update receipt_generator.rb to calculate the tax and totals for all items

--- bin/receipt_generator.rb
+++ bin/receipt_generator.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-def parse_item(line)
-  quantity, *name, _, price = line.split
-
-  {
-    quantity: quantity.to_i,
-    name: name.join(' '),
-    price: price.to_f
-  }
-end
+require_relative '../lib/item'
 
 if ARGV.empty?
-  puts 'Provide input.txt file'
+  puts "\nNo input file provided.\nPlease pass a valid input txt file path as an argument, e.g. 'data/input_1.txt'."
   exit 1
 end
 
@@ -19,11 +11,20 @@ input_file = ARGV[0]
 begin
   input_lines = File.readlines(input_file).map(&:strip)
 rescue Errno::ENOENT
-  puts 'File not found mate'
+  puts "\nFile not found: '#{input_file}'\nPlease ensure the path is correct and the file exists."
   exit
 end
 
 items = input_lines.map do |line|
-  item_data = parse_item(line)
-  puts item_data
+  quantity, *name_parts, _, price = line.split
+  name = name_parts.join(' ')
+
+  Item.new(name, quantity.to_i, price.to_f)
 end
+
+items.each { |item| puts item }
+total_tax = items.sum { |item| item.sales_tax * item.quantity }
+total_price = items.sum(&:total_price)
+
+puts "Sales Taxes: #{format('%.2f', total_tax)}"
+puts "Total: #{format('%.2f', total_price)}"
